affc40b689a7fd57e9f997843bdfee718759dc17
Sonson6 <nelsoncastro@hotmail.fr>
2024-04-22 19:27:05 +0100

    adding docstring to tests

--- tests/test_make_addition.py
+++ tests/test_make_addition.py
@@ -2,10 +2,12 @@ from calculator_api.src.calculator_utils import make_addition
 
 
 def test_make_addition_good_int() -> None:
+    """_summary_"""
     result = make_addition(10, 5)
     assert result == 15
 
 
 def test_make_addition_good_float() -> None:
+    """_summary_"""
     result = make_addition(10, 0.5)
     assert result == 10.5
