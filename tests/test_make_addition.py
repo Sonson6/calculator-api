264e35bad4160833da2fce587d182ed516151f3f
Sonson6 <nelsoncastro@hotmail.fr>
2024-04-22 19:30:21 +0100

    add error to unit tests to check travis behavior

--- tests/test_make_addition.py
+++ tests/test_make_addition.py
@@ -10,4 +10,4 @@ def test_make_addition_good_int() -> None:
 def test_make_addition_good_float() -> None:
     """_summary_"""
     result = make_addition(10, 0.5)
-    assert result == 10.5
+    assert result == 10.1
